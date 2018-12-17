d0fc9d7941732e0077edd3a4e733237f60644e18
Matthias Pfeil <matthias.pfeil@wwu.de>
2018-12-17 12:07:58 +0100

    add basic tests

--- src/index.js
+++ src/index.js
@@ -84,3 +84,5 @@ const startServer = function startServer () {
 };
 
 startServer();
+
+module.exports = app; // for testing
---
+++ test/test.js
@@ -0,0 +1,37 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../src/index');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Compiler', () => {
+  describe('/GET index', () => {
+    it('it should get the index page and answer with a 404 ', (done) => {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+  })
+
+  describe('/POST compiler', () => {
+    it('should post a sketch and compile it', (done) => {
+      const sketch = {
+        board: 'sensebox-mcu',
+        sketch: 'void setup() {\nSerial.begin(9600);\nSerial.println(\"Hello World\");\n}\nvoid loop() {}"}'
+      };
+      chai.request(server)
+        .post('/compile')
+        .send(sketch)
+        .end((err, res) => {
+          console.log(res);
+          done();
+        })
+    });
+  })
+});
