7a3fb41e0d1e469c3965e6b013f85011c74e4de5
Felix Erdmann <hello@felixerdmann.com>
2021-12-09 16:14:30 +0100

    initial arduino-cli implementation

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM debian:9.4-slim as builder
+FROM node:16
 
-ENV IDE_VERSION=1.8.13 \
-  SENSEBOXCORE_VERSION=1.4.2 \
+ENV ARDUINO_CLI_VERSION=0.20.2 \
+  SENSEBOXCORE_VERSION=1.5.0-beta \
   ARDUINO_SAMD_VERSION=1.8.11 \
   ARDUINO_AVR_VERSION=1.8.3 \
   SENSEBOXCORE_URL=https://raw.githubusercontent.com/sensebox/senseBoxMCU-core/master/package_sensebox_index.json \
@@ -13,44 +13,65 @@ ENV IDE_VERSION=1.8.13 \
   GPS_LIBRARY_URL=https://github.com/sparkfun/SparkFun_u-blox_GNSS_Arduino_Library/archive/v2.0.3.zip\
   MQTT_LIBRARY_URL=https://github.com/adafruit/Adafruit_MQTT_Library/archive/2.1.0.zip \
   NEWPING_LIBRARY_URL=https://bitbucket.org/teckel12/arduino-new-ping/downloads/NewPing_v1.9.1.zip \
-  PHYPHOX_LIBRARY_URL=https://github.com/sensebox/phyphox-arduino/archive/refs/heads/master.zip \ 
-  PATH=$PATH:/arduino-ide
+  PHYPHOX_LIBRARY_URL=https://github.com/sensebox/phyphox-arduino/archive/refs/heads/master.zip
 
-RUN apt-get update && apt-get install -y xz-utils unzip wget \
-  && wget http://downloads.arduino.cc/arduino-$IDE_VERSION-linux64.tar.xz \
-  && tar xf arduino-$IDE_VERSION-linux64.tar.xz \
-  && mv arduino-$IDE_VERSION /arduino-ide \
-  && wget -O senseBox_Library.zip $SENSEBOX_LIBRARY_URL \
-  && unzip senseBox_Library.zip -d /arduino-ide/libraries \
+RUN apt-get update && apt-get install -y xz-utils unzip wget
+
+RUN curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=/usr/local/bin sh -s ${ARDUINO_CLI_VERSION}
+
+RUN arduino-cli config init
+
+# aloow unsafe sources (zip, git)
+RUN arduino-cli config set library.enable_unsafe_install true
+
+# update arduino-cli
+RUN arduino-cli core update-index
+
+RUN arduino-cli core install arduino:avr
+
+RUN arduino-cli core install arduino:samd@${ARDUINO_SAMD_VERSION}
+
+
+RUN  wget -O senseBox_Library.zip $SENSEBOX_LIBRARY_URL \
+  && arduino-cli lib install --zip-path senseBox_Library.zip \
   && wget -O telegram_Library.zip $TELEGRAM_LIBRARY_URL \
-  && unzip telegram_Library.zip -d /arduino-ide/libraries \
+  && arduino-cli lib install --zip-path telegram_Library.zip \
   && wget -O arduino_json_Library.zip $ARDUINO_JSON_LIBRARY_URL \
-  && unzip arduino_json_Library.zip -d /arduino-ide/libraries \
+  && arduino-cli lib install --zip-path arduino_json_Library.zip \
   && wget -O ttn_arduino_Library.zip $TTN_ARDUINO_LIBRARY_URL \
-  && unzip ttn_arduino_Library.zip -d /arduino-ide/libraries \
+  && arduino-cli lib install --zip-path ttn_arduino_Library.zip \
   && wget -O bsec_Library.zip $BSEC_LIBRARY_URL \
-  && unzip bsec_Library.zip -d /arduino-ide/libraries \
+  && arduino-cli lib install --zip-path bsec_Library.zip \
   && wget -O gps_Library.zip $GPS_LIBRARY_URL \
-  && unzip gps_Library.zip -d /arduino-ide/libraries \
+  && arduino-cli lib install --zip-path gps_Library.zip \
   && wget -O mqtt_Library.zip $MQTT_LIBRARY_URL \
-  && unzip mqtt_Library.zip -d /arduino-ide/libraries \
+  && arduino-cli lib install --zip-path mqtt_Library.zip \
   && wget -O newping_Library.zip $NEWPING_LIBRARY_URL \
-  && unzip newping_Library.zip -d /arduino-ide/libraries \
+  && arduino-cli lib install --zip-path newping_Library.zip \
   && wget -O phyphox_Library.zip $PHYPHOX_LIBRARY_URL \
-  && unzip phyphox_Library.zip -d /arduino-ide/libraries \
-  && arduino --pref boardsmanager.additional.urls=$SENSEBOXCORE_URL --install-boards sensebox:samd:$SENSEBOXCORE_VERSION \
-  && arduino --install-boards arduino:samd:$ARDUINO_SAMD_VERSION \
-  && arduino --install-boards arduino:avr:$ARDUINO_AVR_VERSION \
-  && mkdir -p /arduino-ide/build-cache \
-  && mv /root/.arduino15/packages /arduino-ide/packages \
-  && apt-get purge -y xz-utils unzip wget \
-  && apt-get autoremove -y \
-  && apt-get clean \
-  && rm -rf arduino-$IDE_VERSION-linux64.tar.xz senseBox_Library.zip /var/lib/apt/lists/* /tmp/* /var/tmp/* \
-  && bash -c 'rm -rf /arduino-ide/{java,lib,reference,examples,arduino,install.sh,revisions.txt,uninstall.sh}' \
-  && find /arduino-ide -type d -name "examples" -exec rm -rf \;
+  && arduino-cli lib install --zip-path phyphox_Library.zip
 
-FROM node:8-slim
+# install arduino avr and libraries for senseBox V1
+RUN arduino-cli lib install Ethernet
+RUN arduino-cli lib install Ethernet2
+RUN arduino-cli lib install Wifi101
+
+RUN wget https://raw.githubusercontent.com/sensebox/home/master/libraries/BMP280.zip
+RUN wget https://raw.githubusercontent.com/sensebox/home/master/libraries/HDC100X.zip
+RUN wget https://raw.githubusercontent.com/sensebox/home/master/libraries/Makerblog_TSL45315.zip
+RUN wget https://raw.githubusercontent.com/sensebox/home/master/libraries/VEML6070.zip
+RUN wget https://raw.githubusercontent.com/sensebox/home/master/libraries/LTR329.zip
+
+RUN arduino-cli lib install --zip-path BMP280.zip
+RUN arduino-cli lib install --zip-path HDC100X.zip
+RUN arduino-cli lib install --zip-path Makerblog_TSL45315.zip
+RUN arduino-cli lib install --zip-path VEML6070.zip
+RUN arduino-cli lib install --zip-path LTR329.zip
+RUN arduino-cli lib install --git-url https://github.com/sensebox/SDS011-select-serial 
+
+# install arduino stuff for senseBox V2
+RUN curl -o /root/.arduino15/package_sensebox_index.json https://raw.githubusercontent.com/sensebox/senseBoxMCU-core/master/package_sensebox_index.json
+RUN arduino-cli --additional-urls https://raw.githubusercontent.com/sensebox/senseBoxMCU-core/master/package_sensebox_index.json core install sensebox:samd
 
 WORKDIR /app
 
@@ -62,8 +83,8 @@ COPY yarn.lock /app
 RUN yarn install --pure-lockfile --production
 
 COPY src /app/src
-COPY --from=builder /arduino-ide /app/src/arduino-ide
+# COPY --from=builder /arduino-ide /app/src/arduino-ide
 
-COPY platform.txt /app/src/arduino-ide/packages/arduino/hardware/samd/1.8.11
+# COPY platform.txt /app/src/arduino-ide/packages/arduino/hardware/samd/1.8.11
 
 CMD ["yarn","start"]
--- src/builder.js
+++ src/builder.js
@@ -1,70 +1,114 @@
-const spawn = require('spawn-promise');
-const tempWrite = require('temp-write');
-const tempy = require('tempy');
-const path = require('path');
-const { HTTPError, rimraf_promise } = require('./utils');
+const spawn = require("spawn-promise");
+const tempWrite = require("temp-write");
+const tempy = require("tempy");
+const path = require("path");
+const { HTTPError, rimraf_promise } = require("./utils");
+const fs = require("fs");
 
 const boardFQBNs = {
-  'sensebox-mcu': 'sensebox:samd:sb:power=on',
-  'sensebox': 'arduino:avr:uno'
+  "sensebox-mcu": "sensebox:samd:sb:power=on",
+  sensebox: "arduino:avr:uno",
 };
 
 const validBoards = Object.keys(boardFQBNs);
 
 const boardBinaryFileextensions = {
-  'sensebox-mcu': 'bin',
-  'sensebox': 'hex'
+  "sensebox-mcu": "bin",
+  sensebox: "hex",
 };
 
 const arduinoIdePath = `${__dirname}/arduino-ide`;
 
 const baseArgs = [
-  '-compile',
-  '-hardware', `${arduinoIdePath}/hardware`,
-  '-hardware', `${arduinoIdePath}/packages`,
-  '-tools', `${arduinoIdePath}/tools-builder`,
-  '-tools', `${arduinoIdePath}/tools`,
-  '-tools', `${arduinoIdePath}/packages`,
-  '-libraries', `${arduinoIdePath}/libraries`,
-  '-build-cache', `${arduinoIdePath}/build-cache`
+  "-compile",
+  "-hardware",
+  `${arduinoIdePath}/hardware`,
+  "-hardware",
+  `${arduinoIdePath}/packages`,
+  "-tools",
+  `${arduinoIdePath}/tools-builder`,
+  "-tools",
+  `${arduinoIdePath}/tools`,
+  "-tools",
+  `${arduinoIdePath}/packages`,
+  "-libraries",
+  `${arduinoIdePath}/libraries`,
+  "-build-cache",
+  `${arduinoIdePath}/build-cache`,
 ];
 
-const payloadValidator = function payloadValidator (req, res, next) {
+const payloadValidator = function payloadValidator(req, res, next) {
   // reject all non application/json requests
-  if (!req.headers['content-type'] || !req.headers['content-type'].startsWith('application/json')) {
-    return next(new HTTPError({ code: 415, error: 'Invalid Content-Type. Only application/json Content-Type allowed.' }));
+  if (
+    !req.headers["content-type"] ||
+    !req.headers["content-type"].startsWith("application/json")
+  ) {
+    return next(
+      new HTTPError({
+        code: 415,
+        error:
+          "Invalid Content-Type. Only application/json Content-Type allowed.",
+      })
+    );
   }
 
   // check if parameters sketch and board are specified and valid
   let { sketch, board } = req.body;
 
   if (!sketch || !board) {
-    return next(new HTTPError({ code: 422, error: 'Parameters \'sketch\' and \'board\' are required' }));
+    return next(
+      new HTTPError({
+        code: 422,
+        error: "Parameters 'sketch' and 'board' are required",
+      })
+    );
   }
 
   sketch = sketch.toString().trim();
   board = board.toString().trim();
 
   if (!sketch || !board) {
-    return next(new HTTPError({ code: 422, error: 'Parameters \'sketch\' and \'board\' are required' }));
+    return next(
+      new HTTPError({
+        code: 422,
+        error: "Parameters 'sketch' and 'board' are required",
+      })
+    );
   }
 
   if (!validBoards.includes(board)) {
-    return next(new HTTPError({ code: 422, error: `Invalid board parameter. Valid values are: ${validBoards.join(',')}` }));
+    return next(
+      new HTTPError({
+        code: 422,
+        error: `Invalid board parameter. Valid values are: ${validBoards.join(
+          ","
+        )}`,
+      })
+    );
   }
 
   req._builderParams = { sketch, board };
   next();
 };
 
-const execBuilder = async function execBuilder ({ board, sketch, buildDir }) {
-  const tmpSketchPath = await tempWrite(sketch, 'sketch.ino');
+const execBuilder = async function execBuilder({ board, sketch, buildDir }) {
+  // const tmpSketchPath = await tempWrite(sketch);
+  const sketchDir = tempy.directory();
+  const sketchName = sketchDir.split("/tmp/")[1];
+  const tmpSketchPath = `${sketchDir}/${sketchName}.ino`;
+  fs.writeFileSync(tmpSketchPath, sketch);
 
-  await spawn(`${arduinoIdePath}/arduino-builder`, [
-    ...baseArgs,
-    '-fqbn', boardFQBNs[board],
-    '-build-path', buildDir,
-    tmpSketchPath
+  console.log(sketch);
+  console.log(
+    `arduino-cli compile --fqbn ${boardFQBNs[board]} --build-path ${buildDir} ${sketchDir}`
+  );
+  await spawn(`arduino-cli compile`, [
+    // ...baseArgs,
+    "--fqbn",
+    boardFQBNs[board],
+    "--build-path",
+    buildDir,
+    sketchDir,
   ]);
 
   try {
@@ -75,9 +119,14 @@ const execBuilder = async function execBuilder ({ board, sketch, buildDir }) {
   }
 };
 
-const compileHandler = async function compileHandler (req, res, next) {
-  if (req.method !== 'POST') {
-    return next(new HTTPError({ code: 405, error: 'Invalid HTTP method. Only POST requests allowed on /compile.' }));
+const compileHandler = async function compileHandler(req, res, next) {
+  if (req.method !== "POST") {
+    return next(
+      new HTTPError({
+        code: 405,
+        error: "Invalid HTTP method. Only POST requests allowed on /compile.",
+      })
+    );
   }
 
   const buildDir = tempy.directory();
@@ -87,17 +136,23 @@ const compileHandler = async function compileHandler (req, res, next) {
   try {
     await execBuilder(req._builderParams);
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-      code: 201,
-      message: 'Sketch successfully compiled and created!',
-      data: {
-        id: buildDir.split('/')[2]
-      }
-    }));
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        code: 201,
+        message: "Sketch successfully compiled and created!",
+        data: {
+          id: buildDir.split("/")[2],
+        },
+      })
+    );
   } catch (err) {
     return next(new HTTPError({ error: err.message }));
   }
-}
+};
 
-module.exports = { payloadValidator, compileHandler, boardBinaryFileextensions };
+module.exports = {
+  payloadValidator,
+  compileHandler,
+  boardBinaryFileextensions,
+};
