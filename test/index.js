89ad7b40298ebd6d9b1d647c3666bb896ae5d89c
Felix Erdmann <hello@felixerdmann.com>
2024-11-13 14:26:02 +0000

    test: board specific test files

---
+++ test/index.js
@@ -0,0 +1,95 @@
+process.env.NODE_ENV = "test";
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../src/index");
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Compiler", () => {
+  describe("/GET index", () => {
+    it("it should get the index page and answer with a 404 ", (done) => {
+      chai
+        .request(server)
+        .get("/")
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+  });
+
+  describe("/compile", () => {
+    it("should reject request without board parameter", (done) => {
+      chai
+        .request(server)
+        .post("/compile")
+        .send({ sketch: 'void setup() {} void loop() {}' })
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.have
+            .property("message")
+            .eql("Parameters 'sketch' and 'board' are required");
+          done();
+        });
+    });
+
+    it("should reject request without sketch parameter", (done) => {
+      chai
+        .request(server)
+        .post("/compile")
+        .send({ board: "sensebox-mcu" })
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.have
+            .property("message")
+            .eql("Parameters 'sketch' and 'board' are required");
+          done();
+        });
+    });
+
+    it("should reject request with invalid board", (done) => {
+      chai
+        .request(server)
+        .post("/compile")
+        .send({ board: "esp8266", sketch: 'void setup() {} void loop() {}' })
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.have
+            .property("message")
+            .eql("Invalid board parameter. Valid values are: sensebox-mcu,sensebox,sensebox-esp32s2");
+          done();
+        });
+    });
+
+    it("should reject request with wrong Content-Type", (done) => {
+      chai
+        .request(server)
+        .post("/compile")
+        .set("Content-Type", "text/plain")
+        .send("")
+        .end((err, res) => {
+          res.should.have.status(415);
+          res.body.should.have
+            .property("message")
+            .eql("Invalid Content-Type. Only application/json Content-Type allowed.");
+          done();
+        });
+    });
+
+    it("should only accept POST request", (done) => {
+      chai
+        .request(server)
+        .get("/compile")
+        .send({ board: "sensebox-mcu", sketch: 'void setup() {} void loop() {}' })
+        .end((err, res) => {
+          res.should.have.status(405);
+          res.body.should.have
+            .property("message")
+            .eql("Invalid HTTP method. Only POST requests allowed on /compile.");
+          done();
+        });
+    });
+  });
+});
---
+++ test/mcu.js
@@ -0,0 +1,54 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../src/index");
+const should = chai.should();
+const fs = require("fs");
+
+
+chai.use(chaiHttp);
+
+describe("Compiler - MCU", () => {
+  let downloadId_mcu = "";
+
+  it("should compile an empty sketch for senseBox MCU", (done) => {
+    const sketch = fs.readFileSync("test/sketches/empty.ino", "utf8");
+
+    chai
+      .request(server)
+      .post("/compile")
+      .send({ board: "sensebox-mcu", sketch })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.data.should.have.property("id");
+        downloadId_mcu = res.body.data.id;
+        done();
+      });
+  });
+
+  it("should compile a hello world sketch for senseBox MCU", (done) => {
+    const sketch = fs.readFileSync("test/sketches/hello-world.ino", "utf8");
+
+    chai
+      .request(server)
+      .post("/compile")
+      .send({ board: "sensebox-mcu", sketch })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.data.should.have.property("id");
+        downloadId_mcu = res.body.data.id;
+        done();
+      });
+  });
+
+  it("should download sketch for senseBox MCU", (done) => {
+    chai
+      .request(server)
+      .get("/download")
+      .query({ board: "sensebox-mcu", id: downloadId_mcu })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.header.should.have.property("content-disposition").eql("attachment; filename=sketch.bin");
+        done();
+      });
+  });
+});
