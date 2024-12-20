620b98b3b8f627d95c6f6794f706e5f891e6278c
Felix Erdmann <hello@felixerdmann.com>
2024-12-20 10:47:48 +0100

    feat: list installed libraries

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
-const connect = require('connect');
-const http = require('http');
-const urlParser = require('url').parse;
-const bodyParser = require('body-parser');
+const connect = require("connect");
+const http = require("http");
+const urlParser = require("url").parse;
+const bodyParser = require("body-parser");
 const app = connect();
-const responseTime = require('response-time');
-const morgan = require('morgan');
+const responseTime = require("response-time");
+const morgan = require("morgan");
 
-const { compileHandler, payloadValidator } = require('./builder');
-const { downloadHandler } = require('./download');
-const { HTTPError } = require('./utils');
+const { compileHandler, payloadValidator } = require("./builder");
+const { downloadHandler } = require("./download");
+const { HTTPError } = require("./utils");
+const { spawnSync } = require("child_process");
 
 const defaultHeaders = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json',
-  'Allow': 'GET,POST',
-  'X-Backend-Server': (require('os').hostname())
+  "Content-Type": "application/json",
+  Accept: "application/json",
+  Allow: "GET,POST",
+  "X-Backend-Server": require("os").hostname(),
 };
 
-const preflight = function preflight (req, res, next) {
+const preflight = function preflight(req, res, next) {
   // preflight POST request https://gist.github.com/balupton/3696140
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Request-Method', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-  res.setHeader('Access-Control-Allow-Headers', 'content-type');
-  res.setHeader('Access-Control-Expose-Headers', 'x-backend-server, x-response-time');
-  if (req.method === 'OPTIONS') {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Expose-Headers",
+    "x-backend-server, x-response-time"
+  );
+  if (req.method === "OPTIONS") {
     res.writeHead(204);
     res.end();
     return;
   }
 
   next();
-}
+};
 
-const preRequestValidator = function preRequestValidator (req, res, next) {
+const preRequestValidator = function preRequestValidator(req, res, next) {
   // set some headers, just in case
   for (const [k, v] of Object.entries(defaultHeaders)) {
     res.setHeader(k, v);
@@ -44,43 +48,80 @@ const preRequestValidator = function preRequestValidator (req, res, next) {
   req._url = url;
 
   // reject everything not coming through /compile or /download
-  if (url.pathname !== '/compile' && url.pathname !== '/download') {
+  if (
+    url.pathname !== "/compile" &&
+    url.pathname !== "/download" &&
+    url.pathname !== "/libraries"
+  ) {
     return next(new HTTPError({ code: 404, error: `Cannot serve ${req.url}` }));
   }
 
   // reject all non POST request
-  if (req.method !== 'POST' && req.method !== 'GET') {
-    return next(new HTTPError({ code: 405, error: 'Invalid HTTP method. Only GET or POST requests allowed.' }));
+  if (req.method !== "POST" && req.method !== "GET") {
+    return next(
+      new HTTPError({
+        code: 405,
+        error: "Invalid HTTP method. Only GET or POST requests allowed.",
+      })
+    );
   }
 
   next();
 };
 
-const errorHandler = function errorHandler (err, req, res, next) {
+const errorHandler = function errorHandler(err, req, res, next) {
   if (res.headersSent) {
     return next(err);
   }
-  res.statusCode = (err.statusCode ? err.statusCode : 500);
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({
-    code: http.STATUS_CODES[res.statusCode],
-    message: err.message
-  }));
+  res.statusCode = err.statusCode ? err.statusCode : 500;
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      code: http.STATUS_CODES[res.statusCode],
+      message: err.message,
+    })
+  );
 };
 
-const startServer = function startServer () {
-  app.use(morgan(':date[iso] :res[x-backend-server] :remote-addr :req[x-real-ip] :method :url :response-time[0] :status', { skip: (req, res) => process.env.NODE_ENV === 'test' }));
+const startServer = function startServer() {
+  app.use(
+    morgan(
+      ":date[iso] :res[x-backend-server] :remote-addr :req[x-real-ip] :method :url :response-time[0] :status",
+      { skip: (req, res) => process.env.NODE_ENV === "test" }
+    )
+  );
   app.use(responseTime());
   app.use(preflight);
   app.use(preRequestValidator);
-  app.use('/compile', bodyParser.json());
-  app.use('/compile', payloadValidator);
-  app.use('/compile', compileHandler);
-  app.use('/download', downloadHandler);
+  app.use("/compile", bodyParser.json());
+  app.use("/compile", payloadValidator);
+  app.use("/compile", compileHandler);
+  app.use("/download", downloadHandler);
+  app.use("/libraries", function (req, res) {
+    // read request parameter format (json or text)
+    const format = req._url.query.format;
+
+    if (format === "json") {
+      const child = spawnSync("arduino-cli", [
+        "lib",
+        "list",
+        "--all",
+        "--format",
+        "json",
+      ]);
+      res.setHeader("Content-Type", "application/json");
+      res.end(child.stdout.toString());
+      return;
+    }
+
+    const child = spawnSync("arduino-cli", ["lib", "list", "--all"]);
+    res.setHeader("Content-Type", "text/plain");
+    res.end(child.stdout.toString());
+  });
   app.use(errorHandler);
 
   http.createServer(app).listen(3000);
-  console.log('Compiler started and listening on port 3000!');
+  console.log("Compiler started and listening on port 3000!");
 };
 
 startServer();
